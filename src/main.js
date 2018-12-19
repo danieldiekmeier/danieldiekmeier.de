38f07b41976bf4752160c49507e78817454aeb1b
Daniel Diekmeier <fax@danieldiekmeier.de>
2018-12-19 14:36:39 +0100

    Update logic for random words

--- src/main.js
+++ src/main.js
@@ -1,23 +1,37 @@
-var descriptions = [
-  'Programmierer',
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle (a) {
+  var j, x, i
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1))
+    x = a[i]
+    a[i] = a[j]
+    a[j] = x
+  }
+  return a
+}
+
+var descriptions = shuffle([
   'Mensch',
   'Softwareentwickler',
   'Front End Entwickler',
   'toller Hecht',
-  'Tausendsassa'
-];
+  'Tausendsassa',
+  'Weltbürger',
+  'HTML Ninja'
+])
 
-var prevIndex = 0;
+var index = 0
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('html').addEventListener('click', function () {
-    var nextIndex = prevIndex
-    while (prevIndex === nextIndex) {
-      nextIndex = Math.floor(Math.random() * descriptions.length);
-    }
-    prevIndex = nextIndex
+document.documentElement.addEventListener('click', function (event) {
+  const isLink = !!event.target.closest('a')
 
-    var description = descriptions[nextIndex];
-    document.querySelector('[data-role="description"]').innerText = description
-  })
+  if (isLink) return
+
+  index = (index + 1) % descriptions.length
+
+  var description = descriptions[index]
+  document.querySelector('[data-role="description"]').innerText = description
 })
